c3d682934da44eeae751fc9f76891f49bfeff445
rishabhkirito <rishabh.santosh@gmail.com>
2025-09-11 23:55:13 +0530

    n^2

--- day_3_dsa_intro.py
+++ day_3_dsa_intro.py
@@ -12,7 +12,14 @@ def find_at_value(data_list,value):
             return item
     return None
 
+def find_all_value(data_list):
+    for item1 in data_list:
+        for item2 in data_list:
+            pass
+
+
 large_list = list(range(10_000_000))
+small_list = list(range(1_000))
 
 start_time_o1 = time.time()
 result_o = find_at_index(large_list,9_000_000)
@@ -23,7 +30,15 @@ print(f"Time Taken is {end_time_o1-start_time_o1}")
 print("\n"+"-"*20+"\n")
 
 start_time_on = time.time()
-result_n = find_at_index(large_list,1_000)
+result_n = find_at_value(large_list,9_999_000)
 end_time_on = time.time()
 print(f"O(n) operation found {result_n}")
 print(f"Time Taken is {end_time_on-start_time_on}")
+
+print("\n"+"-"*20+"\n")
+
+start_time_on2 = time.time()
+result_n2 = find_all_value(small_list)
+end_time_on2 = time.time()
+print(f"O(n^2) operation found {result_n2}")
+print(f"Time Taken is {end_time_on2-start_time_on2}")
