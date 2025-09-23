bd427f47d791aaf30ada6cd65425e09b463c6abb
rishabhkirito <rishabh.santosh@gmail.com>
2025-09-23 19:54:53 +0530

    minor change

--- day_5_recursion.py
+++ day_5_recursion.py
@@ -2,7 +2,8 @@ def recursion(s):
     if len(s) <= 1:
         return s
     
-    return (s[1:]) + s[0]
+    #return (s[1:] + s[0])  this will call the recursion only one time
+    return recursion(s[1:]) + s[0]
    
 recursion_word = input("enter a word: ")
 if recursion_word == exit:
